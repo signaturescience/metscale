b9ee4b7a8bdf89c9b7a4fe0689073f24059b6cbe
cgrahlmann <cgrahlmann@signaturescience.com>
2018-10-29 13:58:43 -0500

    Added options to offline processing.  Added all option to download python script and decided to push offline_downloads.json file

--- workflows/download_offline_files.py
+++ workflows/download_offline_files.py
@@ -6,10 +6,11 @@ import sys
 import argparse
 
 
+selections=['read_filtering', 'test_files', 'all']  #keep all at the end of the list
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Script to download data required for offline processing of Dahak software")
-    parser.add_argument("--workflow", help="Download databases/images for inputed workflow", choices=['read_filtering', 'test_files'], required=True)
+    parser.add_argument("--workflow", help="Download databases/images for inputed workflow", choices=selections, type=str.lower, required=True)
     parser.add_argument("--data_dir", help="directory to copy non image files to", default="data")
     args = parser.parse_args()
     install_dir = args.data_dir
@@ -21,16 +22,19 @@ if __name__ == '__main__':
     except IOError:
         print("Error: config/offline_downloads.json is missing. Exiting")
         sys.exit(1)
-         
-    if user_input in data.keys():
-        for file_name,url in data[user_input].items():
-            file_type = url.partition(":")[0] 
-            if (file_type == 'https' or file_type == 'http'):
-                print("Downloading " +file_name)
-                urllib.request.urlretrieve(url, install_dir+"/"+file_name)
-            elif (file_type == 'docker'):
-                print("Downloading singularity image " +file_name)
-                sing_command = "singularity pull "+url
-                subprocess.run([sing_command], shell=True)
-                os.rename(file_name, "../container_images/"+file_name)
+    
+    if (user_input == 'all'):
+        user_input = selections[0:-1]
+    for selection in user_input:     
+        if selection in data.keys():
+            for file_name,url in data[selection].items():
+                file_type = url.partition(":")[0] 
+                if (file_type == 'https' or file_type == 'http'):
+                    print("Downloading " +file_name)
+                    urllib.request.urlretrieve(url, install_dir+"/"+file_name)
+                elif (file_type == 'docker'):
+                    print("Downloading singularity image " +file_name)
+                    sing_command = "singularity pull "+url
+                    subprocess.run([sing_command], shell=True)
+                    os.rename(file_name, "../container_images/"+file_name)
 
