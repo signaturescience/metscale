99a18388a7bbb13cc622a146a659f8d63316c34a
cgrahlmann <cgrahlmann@signaturescience.com>
2020-06-29 11:15:53 -0500

    Changed args

--- workflows/post_processing/setup_post_processing.py
+++ workflows/post_processing/setup_post_processing.py
@@ -13,23 +13,24 @@ conda install -c r r-rjson
 
 
 
-
+#create the combined_output.json file
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--data", help="input/data dir")
-    parser.add_argument("--post", help="output/data dir")
+    parser.add_argument("--input", help="input/data dir")
+    parser.add_argument("--post", help="post_processing dir")
     args = parser.parse_args()
 
     subprocess.call (["Rscript", "-e", 'library(devtools); install_github("ropensci/taxizedb")' ])
     subprocess.call (["Rscript", "-e", "taxizedb::db_download_ncbi()"])
-    subprocess.call (["Rscript", "-e", "db_download_genbank.R"])
-    subprocess.call (["Rscript", "-e", "db_download_uniprot.R"])
-    subprocess.call (["Rscript", "-e", "add_assembly_lookup.R "])
-    process_str = "process_output.R " + args.input + " " + args.output
-    subprocess.call (["Rscript", "-e", process_str])
+    subprocess.call (["Rscript", "db_download_genbank.R"])
+    subprocess.call (["Rscript", "db_download_uniprot.R"])
+    subprocess.call (["Rscript", "add_assembly_lookup.R "])
+    process_str = "process_output.R " + args.input + " " + args.post
+    subprocess.call (["Rscript", process_str])
     #Rscript -e 'library(devtools); install_github("ropensci/taxizedb")'   #<- this install 0.1.9.93
     #Rscript -e "taxizedb::db_download_ncbi()"
     #Rscript db_download_genbank.R
     #Rscript db_download_uniprot.R
     #Rscript add_assembly_lookup.R 
     #Rscript process_output.R "/data/home/cgrahlmann/metag2/metagenomics/workflows/data" "/data/home/cgrahlmann/metag2/metagenomics/workflows/post_processing"
+    #python setup_post_processing --input /data/home/cgrahlmann/metag2/metagenomics/workflows/data --post /data/home/cgrahlmann/metag2/metagenomics/workflows/post_processing
