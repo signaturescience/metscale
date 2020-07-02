a79f4112c7ed115454a1449f05bd008e35dbca4d
cgrahlmann <cgrahlmann@signaturescience.com>
2020-07-02 11:13:12 -0500

    Split up python script for setup and processing

---
+++ workflows/post_processing/create_combined_output.py
@@ -0,0 +1,17 @@
+import argparse
+import subprocess
+
+
+
+#create combined_output.json
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input", help="input/data dir")
+    parser.add_argument("--post", help="post_processing dir")
+    args = parser.parse_args()
+
+    process_str = "process_output.R " + args.input + " " + args.post
+    subprocess.call (["Rscript", process_str])
+    input_str = args.post + "/combined_output.json"
+    output_str = args.input + "/combined_output.json"
+    subprocess.call(["mv", input_str, output_str])
--- workflows/post_processing/setup_post_processing.py
+++ workflows/post_processing/setup_post_processing.py
@@ -13,7 +13,7 @@ conda install -c r r-rjson
 
 
 
-#create the combined_output.json file
+#download DB's
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--input", help="input/data dir")
@@ -24,11 +24,9 @@ if __name__ == '__main__':
     subprocess.call (["Rscript", "-e", "taxizedb::db_download_ncbi()"])
     subprocess.call (["Rscript", "db_download_genbank.R"])
     subprocess.call (["Rscript", "db_download_uniprot.R"])
-    assembly_str_zip = args.post + "/assemblyAccession_to_taxid.zip"
-    assembly_str_txt = args.post + "/assemblyAccession_to_taxid.txt"
-    subprocess.call (["Rscript", "add_assembly_lookup.R", assembly_str_zip, assembly_str_txt])
+    subprocess.call (["Rscript", "add_assembly_lookup.R "])
     #process_str = "process_output.R " + args.input + " " + args.post
-    subprocess.call (["Rscript", "process_output.R", args.input, args.post])
+    #subprocess.call (["Rscript", process_str])
     #Rscript -e 'library(devtools); install_github("ropensci/taxizedb")'   #<- this install 0.1.9.93
     #Rscript -e "taxizedb::db_download_ncbi()"
     #Rscript db_download_genbank.R
