5f52d62025c85c8b0f724b479cbeeb212e487be2
cgrahlmann <cgrahlmann@signaturescience.com>
2020-07-08 13:51:57 -0500

    working on final report workflow and modified sample files input alg

--- workflows/post_processing/create_combined_output.py
+++ workflows/post_processing/create_combined_output.py
@@ -1,18 +1,25 @@
 import argparse
 import subprocess
+import os
 
 
 
 #create combined_output.json
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--input", help="input/data dir")
-    parser.add_argument("--post", help="post_processing dir")
-    args = parser.parse_args()
-    print(args)
-
-    #process_str = "process_output.R " + args.input + " " + args.post
-    subprocess.call (["Rscript", "process_output.R", args.input, args.post])
-    input_str = args.post + "/combined_output.json"
-    output_str = args.input + "/combined_output.json"
+#    parser = argparse.ArgumentParser()
+#    parser.add_argument("--input", help="input/data dir")
+#    parser.add_argument("--post", help="post_processing dir")
+#    args = parser.parse_args()
+#    print(args)
+#   subprocess.call (["Rscript", "process_output.R", args.input, args.post])
+    input = snakemake.params[0]
+    print("input: "+str(input))
+    post_processing = snakemake.params[1]
+    print("pp: "+str(post_processing))
+    os.chdir(post_processing)
+    print(os.getcwd())
+    path_to_script = os.path.join(post_processing, "process_output.R")
+    subprocess.call (["Rscript", "process_output.R", input, post_processing])
+    input_str = post_processing + "/combined_output.json"
+    output_str = input + "/combined_output.json"
     subprocess.call(["mv", input_str, output_str])
