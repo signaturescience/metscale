e9bd2534e7f8d9c9fd46da4f997379857bf828ad
Chris Grahlmann <cgrahlmann@signaturescience.com>
2021-02-12 14:08:05 -0600

    Saving the short DF instead of the full DF

--- workflows/post_processing/create_abundance_graph.py
+++ workflows/post_processing/create_abundance_graph.py
@@ -269,7 +269,7 @@ def get_all_rows_with_values(abundance_df):
 
 if __name__ == '__main__':
     #parser = argparse.ArgumentParser()
-    #parser.add_argument("--data", help="data dir")
+    #parser.add_argument("--data", help="data dir",default="/data/home/cgrahlmann/metag2/metagenomics/workflows/data/SRR606249_subset10_1_reads_finished/")
     #parser.add_argument("--post", help="post processing dir")
     #args = parser.parse_args()
     #data_path = args.data
@@ -285,8 +285,7 @@ if __name__ == '__main__':
 
     #we are going to sort by - all rows have a value greater than 0.
     sorted_abundance_df = get_all_rows_with_values(abundance_df)
-    sorted_abundance_filepath = os.path.join(data_path, 'signal_plot.tsv')
-    sorted_abundance_df.to_csv(sorted_abundance_filepath, sep='\t')
+    abundance_filepath = os.path.join(data_path, 'signal_plot.tsv')
+    abundance_df.to_csv(abundance_filepath, sep='\t')
 
     create_graph(sorted_abundance_df, workflow_cols, data_path)  
-    #print("Done")
