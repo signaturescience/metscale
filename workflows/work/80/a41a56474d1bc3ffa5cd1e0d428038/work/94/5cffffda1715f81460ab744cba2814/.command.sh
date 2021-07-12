a59afaa452646e54d9697e7822740438ec24ad43
Matthew Scholz <mscholz@signaturescience.com>
2021-07-12 15:08:11 -0500

    cleanup work2

--- workflows/work/80/a41a56474d1bc3ffa5cd1e0d428038/work/94/5cffffda1715f81460ab744cba2814/.command.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash -ue
-mkdir -p /tmp/SRR606249_subset10_1_reads_trim2_prokka.megahit_seqscreen_seqscreen_db/reportgeneration2
--- workflows/work/89/54ade5bd58a8b00351209955963fe5/work/74/d2c235a43414ac97bdd0f8c251452b/.command.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash -ue
-echo -n " # Launching taxonomic_identification workflow ...... " | tee -a /tmp/SRR606249_subset10_1_reads_trim2_prokka.megahit_seqscreen_seqscreen_db/reportgeneration2/seqscreen.log; date '+%H:%M:%S %Y-%m-%d' | tee -a /tmp/SRR606249_subset10_1_reads_trim2_prokka.megahit_seqscreen_seqscreen_db/reportgeneration2/seqscreen.log
