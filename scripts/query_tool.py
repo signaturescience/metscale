979a27a8d43c09d9c86442a547cb51b65b794022
shakeeljessa <shakeel@utexas.edu>
2021-08-27 10:59:00 -0500

    sort all -CMO output

--- scripts/query_tool.py
+++ scripts/query_tool.py
@@ -957,7 +957,7 @@ def containment_dict_show_build_plan(source_file_tuples, contain, hide_older_ref
         sort_all_db_names_2 = sorted(sort_all_db_names_2, key=lambda x: int(x.lower().replace("refseq_v", "")), reverse=True)
         sort_all_db_names = sort_all_db_names_1 + sort_all_db_names_2
 	
-        for nm in all_db_names:
+        for nm in sort_all_db_names:
             rpt_nm = nm; rpt_incon = ''; rpt_insrc = ''; rpt_status = '';
 
             if not nm in source_file_tuples_dict:   # name in containment_dict.json, not in source_list
